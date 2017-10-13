36bad652eadb3ba962b1654c01b72c6cdf584f11
Imre Kiss <kissi.szeged@partner.samsung.com>
2017-10-13 03:25:42 +0200

    Add options to eslintrc.js. (#19)
    IoT.jsCode-DCO-1.0-Signed-off-by: Imre Kiss kissi.szeged@partner.samsung.com

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-  "extends": "eslint:recommended"
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "jquery": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-console": "off",
+    "no-var": 2,
+    "max-len": ["error", {
+      "code": 120,
+      "ignoreUrls": true,
+      "ignoreTemplateLiterals": true,
+      "ignoreRegExpLiterals": true
+    }]
+  }
 }
