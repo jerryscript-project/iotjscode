b0e69fc0518a2e42a4e733405018271a3d44ab59
Imre Kiss <kissi.szeged@partner.samsung.com>
2017-10-20 04:26:07 +0200

    Rework the multimap module based on ES6. (#27)
    IoT.jsCode-DCO-1.0-Signed-off-by: Imre Kiss kissi.szeged@partner.samsung.com

--- src/app/client-multimap.js
+++ src/app/client-multimap.js
@@ -15,64 +15,58 @@
 
 import Util from './util';
 
-/**
- * Constructor.
- */
-function Multimap() {
-  if (!(this instanceof Multimap)) {
-    throw new TypeError("Multimap constructor cannot be called as a function.");
+export default class Multimap {
+
+  constructor() {
+    this._map = {};
   }
 
-  this._map = {};
-}
-
-/**
- * Returns an item from the map.
- *
- * @param {mixed} key Key of the searched item.
- * @return {mixed} The item array if that is exists, empty array otherwise.
- */
-Multimap.prototype.get = function(key) {
-  var item = this._map[key];
-  return item ? item : [];
-}
-
-/**
- * Puts a key-value pair into the map.
- *
- * @param {number} key The key of the new item.
- * @param {mixed} value The new value which will be stored as an array.
- */
-Multimap.prototype.insert = function(key, value) {
-  var item = this._map[key];
-
-  if (item) {
-    item.push(value);
-    return;
+  /**
+   * Returns an item from the map.
+   *
+   * @param {mixed} key Key of the searched item.
+   * @return {mixed} The item array if that is exists, empty array otherwise.
+   */
+  get(key) {
+    let item = this._map[key];
+    return item ? item : [];
   }
 
-  this._map[key] = [value];
+  /**
+   * Puts a key-value pair into the map.
+   *
+   * @param {number} key The key of the new item.
+   * @param {mixed} value The new value which will be stored as an array.
+   */
+  insert(key, value) {
+    let item = this._map[key];
+
+    if (item) {
+      item.push(value);
+      return;
+    }
+
+    this._map[key] = [value];
+  }
+
+  /**
+   * Removes an item at the key position.
+   *
+   * @param {number} key The top array key.
+   * @param {mixed} key The value which will be removed.
+   */
+  delete(key, value) {
+    let array = this._map[key];
+
+    Util.assert(array);
+
+    let newLength = array.length - 1;
+    let i = array.indexOf(value);
+
+    Util.assert(i !== -1);
+
+    array.splice(i, 1);
+
+    array.length = newLength;
+  }
 }
-
-/**
- * Removes an item at the key position.
- *
- * @param {number} key The top array key.
- * @param {mixed} key The value which will be removed.
- */
-Multimap.prototype.delete = function(key, value) {
-  var array = this._map[key];
-
-  Util.assert(array);
-
-  var newLength = array.length - 1;
-  var i = array.indexOf(value);
-
-  Util.assert(i != -1);
-
-  array.splice(i, 1);
-
-  array.length = newLength;
-}
-
-export default Multimap;
